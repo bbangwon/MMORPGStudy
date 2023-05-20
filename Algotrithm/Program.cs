0908a31ad2d94fe839c84491fe10302c01d22ca0
bbangwon <bbangwon.chung@gmail.com>
2023-05-20 16:56:52 +0900

    Board 클래스 맵 만들기

--- Algotrithm/Program.cs
+++ Algotrithm/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+            board.Initialize(25);
+
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
+            
 
             int lastTick = 0;
             while (true)
@@ -24,16 +27,8 @@
 
                 //렌더링
                 Console.SetCursorPosition(0, 0);
+                board.Render();
 
-                for (int i = 0; i < 25; i++) 
-                { 
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
             }
         }
     }
