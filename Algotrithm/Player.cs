b181b4a42c043dcddf97b3d5025f100497208bb8
youngil <youngil@placeb.com>
2023-05-22 18:02:04 +0900

    다익스트라 알고리즘 적용

--- Algotrithm/Player.cs
+++ Algotrithm/Player.cs
@@ -39,6 +39,64 @@
 
             _board = board;
 
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> queue = new Queue<Pos>();
+            queue.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (queue.Count > 0)
+            {
+                Pos pos = queue.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+                    if(nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+
+                    if (_board.Tile![nextY, nextX] == Board.TileType.Wall)
+                        continue;
+
+                    if (found[nextY, nextX])
+                        continue;
+
+                    queue.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+
+            while (parent[y, x].Y != y || parent[y, x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+        }
+
+        void RightHand()
+        {
             // 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
             int[] frontY = new int[] { -1, 0, 1, 0 };
             int[] frontX = new int[] { 0, -1, 0, 1 };
@@ -49,7 +107,7 @@
             _points.Add(new Pos(PosY, PosX));
 
             //목적지에 도착하기 전에는 계속 실행
-            while (PosX != board.DestY || PosX != board.DestX)
+            while (PosX != _board.DestY || PosX != _board.DestX)
             {
                 // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인
                 if (_board.Tile![PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
@@ -80,7 +138,7 @@
             }
         }
 
-        const int MOVE_TICK = 10;
+        const int MOVE_TICK = 100;
         int _sumTick = 0;
         int _lastIndex = 0;
         public void Update(int deltaTick)
--- Exercies/Program.cs
+++ Exercies/Program.cs
@@ -4,12 +4,12 @@
     {
         int[,] adj = new int[6, 6]
         {
-            { 0, 1, 0, 1, 0, 0 },
-            { 1, 0, 1, 1, 0, 0 },
-            { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 1, 0 },
-            { 0, 0, 0, 1, 0, 1 },
-            { 0, 0, 0, 0, 1, 0 },
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 05, 10, -1, -1 },
+            { -1, 05, -1, -1, -1, -1 },
+            { 35, 10, -1, -1, 05, -1 },
+            { -1, -1, -1, 05, -1, 05 },
+            { -1, -1, -1, -1, 05, -1 },
         };
 
         List<int>[] adj2 = new List<int>[]
@@ -34,7 +34,7 @@
             for (int next = 0; next < 6; next++)
             {
                 //연결되어 있지 않으면 스킵
-                if (adj[now, next] == 0)
+                if (adj[now, next] == -1)
                     continue;
 
                 //이미 방문했으면 스킵
@@ -95,7 +95,7 @@
                 for (int next = 0; next < 6;next++)
                 {
                     //인접하지 않았으면 스킵
-                    if (adj[now, next] == 0)
+                    if (adj[now, next] == -1)
                         continue;
 
                     //이미 발견했다면 스킵
@@ -111,7 +111,67 @@
 
         }
 
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];    //최단거리
+            int[] parent = new int[6];
+            Array.Fill(distance, int.MaxValue); //distance를 0으로 초기화하면 실제거리 0과 헷갈림
 
+            distance[start] = 0;
+            parent[start] = start;
+
+            while (true)
+            {
+                //제일 좋은 후보를 찾는다 (가장 가까이에 있는)
+
+                //가장 유력한 후보의 거리와 번호를 저장한다.
+                int closest = int.MaxValue;
+                int now = -1;
+
+                for (int i = 0;i<6;i++)
+                {
+                    if (visited[i])
+                        continue;
+
+                    //아직 발견된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == int.MaxValue || distance[i] >= closest)
+                        continue;
+
+                    //가장 좋은 후보. 정보 갱신
+                    closest = distance[i];
+                    now = i;
+                }
+
+                // 다음 후보가 하나도 없다 -> 종료. 모든점을 다 찾았거나, 연결이 단절되거나
+                if (now == -1)
+                    break;
+
+                //제일 좋은 후보를 찾았으니 방문
+                visited[now] = true;
+
+                //방문한 정점과 인접한 정점들을 조사해서, 상황에 따라 발견한 최단거리를 갱신
+                for (int next = 0; next < 6; next++)
+                {
+                    //연결되지 않은 정점 스킵
+                    if (adj[now, next] == -1)
+                        continue;
+
+                    //이미 방문한 정점은 스킵
+                    if (visited[next])
+                        continue;
+                    
+                    //새로 조사된 정점의 최단거리를 계산
+                    int nextDist = distance[now] + adj[now, next];
+                    //기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면, 정보를 갱신
+                    if(nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
+        }
     }
 
     class Program
@@ -121,7 +181,7 @@
             // DFS (Depth First Search : 깊이 우선 탐색)
             // BFS (Breadth First Search : 너비 우선 탐색)
             Graph graph = new Graph();
-            graph.BFS(0);
+            graph.Dijikstra(0);
         }
     }
 }
