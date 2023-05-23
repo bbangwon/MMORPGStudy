7d2864bd569ab1a8ff092b2992c50510a9093c80
youngil <youngil@placeb.com>
2023-05-23 12:58:41 +0900

    트리 추가

--- Exercies/Program.cs
+++ Exercies/Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace Exercies
 {
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+    }
+
     class Graph
     {
         int[,] adj = new int[6, 6]
@@ -176,12 +182,78 @@
 
     class Program
     {       
-        static void Main(string[] args)
+        static void GraphDemo()
         {
+
             // DFS (Depth First Search : 깊이 우선 탐색)
             // BFS (Breadth First Search : 너비 우선 탐색)
             Graph graph = new Graph();
             graph.Dijikstra(0);
+        }
+
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+            }
+
+            return root;
+        }
+
+        //재귀함수
+        static void PrintTree(TreeNode<string> root)
+        {
+            Console.WriteLine(root.Data);
+            foreach (var child in root.Children)
+                PrintTree(child);
+        }
+
+        //트리 높이
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+
+            foreach (var child in root.Children)
+            {
+                int newHeight = GetHeight(child) + 1;
+                height = Math.Max(height, newHeight);
+            }               
+
+            return height;
+        }
+
+        static void TreeDemo()
+        {
+            TreeNode<string> tree = MakeTree();
+            PrintTree(tree);
+            Console.WriteLine(GetHeight(tree));
+        }
+
+        static void Main(string[] args)
+        {
+            TreeDemo();
         }
     }
 }
